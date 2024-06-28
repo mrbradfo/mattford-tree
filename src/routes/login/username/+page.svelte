6df60b3a0813c985ee00988e819fc38ded873bf9
Matt <mattford@showit.co>
2024-06-27 23:31:41 -0700

    adding username page

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,5 +1,74 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+  import AuthCheck from "$lib/components/AuthCheck.svelte";
+  import { db, user } from "$lib/firebase";
+  import { doc, getDoc, writeBatch} from "firebase/firestore";
+
+    let username = "";
+    let loading = false;
+    let isAvailable = false;
+
+    let debounceTimer: NodeJS.Timeout;
+    async function checkAvailability() {
+        isAvailable = false;
+        clearTimeout(debounceTimer); 
+
+        loading = true;
+        debounceTimer = setTimeout(async () => {
+            console.log("Checking availability of ", username);
+
+            const ref = doc(db, "usernames", username);
+            const exists = await getDoc(ref).then((doc) => {
+                console.log("usernames found:" , doc.data());
+                return doc.exists()
+            });
+            isAvailable = !exists;
+            loading = false;
+        }, 500);
+    }
+
+
+
+    async function confirmUsername() {
+    console.log("confirming username", username);
+    const batch = writeBatch(db);
+    batch.set(doc(db, "usernames", username), { uid: $user?.uid });
+    batch.set(doc(db, "users", $user!.uid), { 
+      username, 
+      photoURL: $user?.photoURL ?? null,
+      published: true,
+      bio: 'I am the Walrus',
+      links: [
+        {
+          title: 'Test Link',
+          url: 'https://kung.foo',
+          icon: 'custom'
+        }
+      ]
+    });
+
+    await batch.commit();
+
+    username = '';
+    isAvailable = false;
+
+  }
+
+
 </script>
 
-<h2> Username </h2>
+<AuthCheck>
+    <h2> Username </h2>
+    <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+        <input
+        type="text"
+        placeholder="Username"
+        class="btn btn-success"
+        bind:value={username}
+        on:input={checkAvailability}
+        />
+
+        <p>Is available? {isAvailable}</p>
+        <button class="btn btn-success">Confirm username @{username} </button>
+
+    </form>
+</AuthCheck>
